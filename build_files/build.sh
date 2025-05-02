3fc71c239070fc89e03317628194e34d8a7f6726
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-05-02 19:07:27 +0300

    feat: Add more packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y alacritty tailscale wireguard-toos
+dnf5 install -y alacritty tailscale wireguard-toos syncthing edk2-ovmf
+
+dnf5 install -y libvirt libvirt-daemon-config-network libvirt-daemon-kvm \
+    qemu-kvm virt-manager virt-viewer
 
 # Use a COPR Example:
 #
@@ -22,3 +25,4 @@ dnf5 install -y alacritty tailscale wireguard-toos
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable libvirtd
