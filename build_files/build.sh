eda9883871e781590f9b84d8642a0196c278bc79
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-05-02 19:43:08 +0300

    feat: Add Nix installation

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,23 @@
 
 set -ouex pipefail
 
+NIX_INSTALLER_VERSION="v3.4.1"
+INSTALLER_URL="https://github.com/determinateSystems/nix-installer/releases/download/${NIX_INSTALLER_VERSION}/nix-installer-x86_64-linux"
+INSTALLER_PATH="/tmp/nix-installer"
+EXPECTED_SHA256="b174ba0340f220ff8be63e8178177d5d46deff42c0ad88d66833f6ba2befbf86"
+
+# Download Nix Determinate Systems installer
+curl -fsSL -o "${INSTALLER_PATH}" "${INSTALLER_URL}"
+chmod +x "${INSTALLER_PATH}"
+
+# Verify the checksum
+echo "${EXPECTED_SHA256}  ${INSTALLER_PATH}" | sha256sum -c -
+
+# Install Nix
+"${INSTALLER_PATH}" install --no-confirm
+
+# Clean up
+rm -f "${INSTALLER_PATH}"
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -21,6 +38,10 @@ dnf5 install -y libvirt libvirt-daemon-config-network libvirt-daemon-kvm \
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+# Install nix
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix |
+    sh -s -- install --no-confirm
 
 #### Example for enabling a System Unit File
 
