4c25befaf44e9eba8c01e7c6f898da59d921f33b
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-05-02 20:40:14 +0300

    fix: Remove nix installation (not supported on ostree-enabled systems

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,25 +2,6 @@
 
 set -ouex pipefail
 
-NIX_INSTALLER_VERSION="v3.4.1"
-INSTALLER_URL="https://install.determinate.systems/nix/tag/${NIX_INSTALLER_VERSION}"
-INSTALLER_PATH="/tmp/nix-installer.sh"
-EXPECTED_SHA256="7df0fa6567434afd2bc4070fc789c7f431bf6392090d69b4e1a4a1e13a181a66"
-
-# Download Nix Determinate Systems installer scrpt
-curl --proto '=https' --tlsv1.2 -sSf -L -o "${INSTALLER_PATH}" "${INSTALLER_URL}"
-chmod +x "${INSTALLER_PATH}"
-
-# Verify the checksum
-echo "${EXPECTED_SHA256}  ${INSTALLER_PATH}" | sha256sum -c -
-
-# Install Nix
-"${INSTALLER_PATH}" install ostree --no-confirm -- --no-start-daemon
-
-# Clean up
-rm -f "${INSTALLER_PATH}"
-### Install packages
-
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
