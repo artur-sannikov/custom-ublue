1369046d9c45fa9dd19ed23f152d79ed5644a647
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-05-05 10:47:01 +0300

    refactor: Install tailscale by adding repo (see Aurora builds)

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,32 +2,23 @@
 
 set -ouex pipefail
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Copy files to container
+rsync -rvK /ctx/system_files/ /
 
-# this installs a package from fedora repos
+# Install tailscale
+dnf5 install tailscale
+
+# Install base packages
 dnf5 install -y alacritty wireguard-tools syncthing edk2-ovmf
 
+# Install libvirt
 dnf5 install -y libvirt libvirt-daemon-config-network libvirt-daemon-kvm \
     qemu-kvm virt-manager virt-viewer
 
-# Install tailscale according to official instructions
-dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-dnf5 install tailscale
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
+# Required for nix installation later
 mkdir /nix
 
-#### Example for enabling a System Unit File
-
+# Enable systemd units
 systemctl enable podman.socket
 systemctl enable libvirtd
 systemctl enable tailscaled
